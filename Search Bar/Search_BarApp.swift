263738cd4c1e6864e853cc141f1edfd1e68dfa0e
Ermanno Fissore <ermannofissore@gmail.com>
2022-10-04 18:10:00 +0100

    Initial Commit

---
+++ Search Bar/Search_BarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Search_BarApp.swift
+//  Search Bar
+//
+//  Created by Ermanno Fissore on 04/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Search_BarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
