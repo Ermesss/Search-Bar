1c79b444872d00b70d57bf5d72e61f3d5a4fcce9
Ermanno Fissore <ermannofissore@gmail.com>
2022-10-05 17:47:00 +0100

    Missing return in instance method expected to return 'UISearchBar'
    Not working

--- Search Bar/ContentView.swift
+++ Search Bar/ContentView.swift
@@ -8,19 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var datas = ["Hello","hi","Welcome","World"]
+    @State var txt = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+           
+         searchView(txt: $txt)
+            
+            List(datas.filter{txt == "" ? true : $0.localizedCaseInsensitiveContains(txt)},id: \.self){i in
+                
+                Text(i).fontWeight(.heavy)
+                
+            }
+        
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct searchView : UIViewRepresentable {
+    func makeUIView(context: Context) -> UISearchBar {
+    }
+    
+    
+    @Binding var txt : String
+    
+    func makeCoordinator() -> Coordinator {
+        return searchView.Coordinator(parent1: self)
+    }
+    
+    
+    func makeUIview(context: UIViewRepresentableContext<searchView>) -> UISearchBar {
+        
+        let searchbar = UISearchBar()
+        searchbar.barStyle = .default
+        searchbar.autocapitalizationType = .none
+        searchbar.delegate = context.coordinator
+        return searchbar
+        
+    }
+    
+    func updateUIView(_ uiView: UISearchBar, context: UIViewRepresentableContext<searchView>) {
+    
+    }
+    
+    class Coordinator : NSObject,UISearchBarDelegate{
+        
+        var parent : searchView!
+        
+        init(parent1 : searchView){
+            parent = parent1
+            
+        }
+        
+        func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+            
+            parent.txt = searchText
+            
+        }
     }
 }
